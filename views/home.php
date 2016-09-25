80109454e7de49f2f911e2f7e2ee9c5e1b3eed2f
sygelyco-projet <vaneckkouakam@gmail.com>
2016-09-25 17:44:55 +0100

    jai cre content header pour corriger le pbs de redirection

--- views/home.php
+++ views/home.php
@@ -3,9 +3,9 @@
  require '../controller/session.php';
   if (!isset($_SESSION['user']))
 {
-$url=$home_path.'/index.php?lang='.$_GET['lang']; 
-//echo '<script>window.location.href ="'.$url.'";</script>';
-  
+if(!isset($_GET['lang']))  $_GET['lang']='fr'; 
+$url='../index.php?lang='.$_GET['lang']; 
+header('Refresh:0;'.$url.'');
 }
   ?>
 <!DOCTYPE html>
@@ -50,121 +50,19 @@ $url=$home_path.'/index.php?lang='.$_GET['lang'];
 
   <!-- container section start -->
   <section id="container" class="">
-   <?php include("../lang/decide-lang.php"); if(!isset($_GET['lang']))  $_GET['lang']='fr'; ?>
-     <?php include("header.php"); ?>
-	 <?php include("menu.php"); // charge dynamiquement en fonction des droits    ?>
-
-      
-      <!--main content start-->
-      <section id="main-content">
-          <section class="wrapper">            
-              <!--overview start-->
-			  <div class="row">
-				<div class="col-lg-12">
-					<h3 class="page-header"><i class="fa fa-laptop"></i> <?php echo tableau_de_bord; ?></h3>
-					<ol class="breadcrumb">
-						<li><i class="fa fa-home"></i><a href="home.php?lang=<?php echo $_GET['lang']; ?>"><?php echo menu1; ?></a></li>
-						<li><i class="fa fa-laptop"></i><?php echo tableau_de_bord; ?></li>						  	
-					</ol>
-				</div>
-			</div>
-              
-            <div class="row">
-				<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-					<div class="info-box blue-bg">
-						<i class="fa fa-cloud-download"></i>
-						<div class="count"><span class="profile-ava">
-                                <img alt="" src="../Public_files/img/avatar1_small.jpg">
-                            </span></div>
-						<div class="title"><?php echo a_propos_lycee; ?></div>						
-					</div><!--/.info-box-->			
-				</div><!--/.col-->
-				
-				<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-					<div class="info-box brown-bg">
-						<i class="fa fa-shopping-cart"></i>
-						<div class="count"><span class="profile-ava">
-                                <img alt="" src="../Public_files/img/avatar1_small.jpg">
-                            </span></div>
-						<div class="title"><?php echo a_propos_proviseur; ?></div>						
-					</div><!--/.info-box-->			
-				</div><!--/.col-->	
-				
-				<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-					<div class="info-box dark-bg">
-						<i class="fa fa-thumbs-o-up"></i>
-						<div class="count">80%</div>
-						<div class="title"><?php echo taux_reussite; ?></div>						
-					</div><!--/.info-box-->			
-				</div><!--/.col-->
-				
-				<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-					<div class="info-box green-bg">
-						<i class="fa fa-cubes"></i>
-						<div class="count">1.426</div>
-						<div class="title"><?php echo moyenne_generale; ?></div>						
-					</div><!--/.info-box-->			
-				</div><!--/.col-->
-				
-				
-			</div><!--/.row-->
-		
-		
-		<div class="row">
-			<div class="col-md-6 portlets">
-            <div class="panel panel-default">
-				<div class="panel-heading">
-                  <h2><strong><?php echo calandrier; ?></strong></h2>
-				<div class="panel-actions">
-                    <a href="#" class="wminimize"><i class="fa fa-chevron-up"></i></a> 
-                    <a href="#" class="wclose"><i class="fa fa-times"></i></a>
-                </div>  
-                 
-                </div><br><br><br>
-                <div class="panel-body">
-                  <!-- Widget content -->
-                  
-                    <!-- Below line produces calendar. I am using FullCalendar plugin. -->
-                    <div id="calendar"></div>
-                  
-                </div>
-              </div> 
-               
-            </div>
-			
-				 <div class="col-md-6 portlets">
-              <div class="panel panel-default">
-                <div class="panel-heading">
-                  <div class="pull-left"><?php echo actualites; ?></div>
-                  <div class="widget-icons pull-right">
-                    <a href="#" class="wminimize"><i class="fa fa-chevron-up"></i></a> 
-                    <a href="#" class="wclose"><i class="fa fa-times"></i></a>
-                  </div>  
-                  <div class="clearfix"></div>
-                </div>
-                <div class="panel-body">
-                  <div class="padd">
-                  
-
-                  </div>
-                  <div class="widget-foot">
-                    <!-- Footer goes here -->
-                  </div>
-                </div>
-              </div>
-              
-            </div>
-                        
-          </div> 
-              <!-- project team & activity end -->
-
-          </section>
-      </section>
-      <!--main content end-->
-  </section>
-  <!-- container section start -->
   
-    <?php include("../footer_index.php"); ?>
+   <?php 
+   if (isset($_SESSION['user'])) {
+   include("../lang/decide-lang.php"); if(!isset($_GET['lang']))  $_GET['lang']='fr'; 
+   include("header.php"); 
+   include("menu.php"); // charge dynamiquement en fonction des droits   
+   include("content_home.php"); // charge dynamiquement en fonction des droits   
+	}
+   ?>
+  
+  </section>
+   <!-- container section start -->  
+   <?php  if (isset($_SESSION['user'])) include("../footer_index.php"); ?>
 
     <!-- javascripts -->
     <script src="../public_files/js/jquery.js"></script>
