7bbd7ec08c5605b5d8f0bc654e5960f425ef64b2
domche <domchemichael@yahoo.com>
2016-09-26 19:22:40 +0200

    jai juste change laccueil du home

--- views/home.php
+++ views/home.php
@@ -33,6 +33,7 @@
     <link href="../public_files/css/style-responsive.css" rel="stylesheet" />
 	<link href="../public_files/css/xcharts.min.css" rel=" stylesheet">	
 	<link href="../public_files/css/jquery-ui-1.10.4.min.css" rel="stylesheet">
+	<link href="../public_files/css/sb-admin-2.css" rel="stylesheet">
 		<script src="../Public_files/js/jquery-1.8.3.min.js"></script>
   </head>
 
@@ -42,7 +43,6 @@
    <?php include("../lang/decide-lang.php"); ?>
      <?php include("header.php"); ?>
 	 <?php include("menu.php"); // charge dynamiquement en fonction des droits    ?>
-
       
       <!--main content start-->
       <section id="main-content">
@@ -57,94 +57,267 @@
 					</ol>
 				</div>
 			</div>
-              
+
+			                      <!--carousel start-->
+                      <section class="panel">
+                          <div style="color:#FFF; background-color:#666" id="c-slide" class="carousel slide auto panel-body">
+                              <ol class="carousel-indicators out">
+                                  <li class="active" data-slide-to="0" data-target="#c-slide"></li>
+                                  <li class="" data-slide-to="1" data-target="#c-slide"></li>
+                                  <li class="" data-slide-to="2" data-target="#c-slide"></li>
+								  <li class="" data-slide-to="3" data-target="#c-slide"></li>
+								  <li class="" data-slide-to="4" data-target="#c-slide"></li>
+                              </ol>
+                              <div class="carousel-inner">
+                                  <div class="item text-center active">
+                                      <h3><b>" L'originalité n'est rien qu'une judicieuse imagination. "</b></h3>
+                                      <small class="">By Voltaire <img style="height:20px; width:20px" alt="" src="../public_files/img/plume.png"></small>
+                                  </div>
+								  <div class="item text-center">
+                                      <h3> <b>" Les deux secrets d'un succès : la Qualité et la Créativité. "</b></h3>
+                                      <small class="">By DAK-OSFT <img style="height:20px; width:20px" alt="" src="../public_files/img/plume.png"></small>
+                                  </div>
+                                  <div class="item text-center">
+                                      <h3><b> " La créativité c’est percer le banal pour trouver le merveilleux. " </b></h3>
+                                      <small class="">By BILL MOYERS <img style="height:20px; width:20px" alt="" src="../public_files/img/plume.png"></small>
+                                  </div>
+								  <div class="item text-center">
+                                      <h3> <b>" Créer vos établissements et laisser nous le numériser avec une créativité sans pareil. "</b></h3>
+                                      <small class="">By DAK-OSFT <img style="height:20px; width:20px" alt="" src="../public_files/img/plume.png"></small>
+                                  </div>
+								  <div class="item text-center">
+                                      <h3> <b>" Concevoir c’est bien mai réaliser c’est encore mieux. "</b></h3>
+                                      <small class="">By DAK-OSFT <img style="height:20px; width:20px" alt="" src="../public_files/img/plume.png"></small>
+                                  </div>
+                              </div>
+                              <a data-slide="prev" href="#c-slide" class="left carousel-control">
+                                  <i class="arrow_carrot-left_alt2"></i>
+                              </a>
+                              <a data-slide="next" href="#c-slide" class="right carousel-control">
+                                  <i class="arrow_carrot-right_alt2"></i>
+                              </a>
+                          </div>
+                      </section>
+                      <!--carousel end-->
+					<!-- Modal -->
+                      
+			<!-- les carre -->
             <div class="row">
-				<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-					<div class="info-box blue-bg">
-						<i class="fa fa-cloud-download"></i>
-						<div class="count"><span class="profile-ava">
-                                <img alt="" src="Public_files/img/avatar1_small.jpg">
-                            </span></div>
-						<div class="title"><?php echo a_propos_lycee; ?></div>						
-					</div><!--/.info-box-->			
-				</div><!--/.col-->
-				
-				<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-					<div class="info-box brown-bg">
-						<i class="fa fa-shopping-cart"></i>
-						<div class="count"><span class="profile-ava">
-                                <img alt="" src="Public_files/img/avatar1_small.jpg">
-                            </span></div>
-						<div class="title"><?php echo a_propos_proviseur; ?></div>						
-					</div><!--/.info-box-->			
-				</div><!--/.col-->	
-				
-				<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-					<div class="info-box dark-bg">
-						<i class="fa fa-thumbs-o-up"></i>
-						<div class="count">80%</div>
-						<div class="title"><?php echo taux_reussite; ?></div>						
-					</div><!--/.info-box-->			
-				</div><!--/.col-->
-				
-				<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
-					<div class="info-box green-bg">
-						<i class="fa fa-cubes"></i>
-						<div class="count">1.426</div>
-						<div class="title"><?php echo moyenne_generale; ?></div>						
-					</div><!--/.info-box-->			
-				</div><!--/.col-->
-				
-				
-			</div><!--/.row-->
-		
-		
-		<div class="row">
-			<div class="col-md-6 portlets">
-            <div class="panel panel-default">
-				<div class="panel-heading">
-                  <h2><strong><?php echo calandrier; ?></strong></h2>
-				<div class="panel-actions">
-                    <a href="#" class="wminimize"><i class="fa fa-chevron-up"></i></a> 
-                    <a href="#" class="wclose"><i class="fa fa-times"></i></a>
-                </div>  
-                 
-                </div><br><br><br>
-                <div class="panel-body">
-                  <!-- Widget content -->
-                  
-                    <!-- Below line produces calendar. I am using FullCalendar plugin. -->
-                    <div id="calendar"></div>
-                  
+                <div class="col-lg-3 col-md-6">
+                  <div class="panel panel-bleu">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-tasks fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div style="font-family:'Times New Roman', Times, serif; font-size:22px" class="huge">Module 1</div>
+                                    <div>Lycée</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a data-toggle="modal" data-target="#myModal">
+                            <div class="panel-footer">
+                                <span class="pull-left">Voir Détails</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div> 
                 </div>
-              </div> 
-               
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <h4 class="modal-title" id="myModalLabel">Module 1 : Gestion de vos lycée</h4>
+      </div>
+      <div class="modal-body">
+       <pre class="prettyprint linenums">
+<h5 align="center"><b>Bienvenue dans la configuration de vos établissements</b></h5>
+Ce module vous permet entre autre de :
+1.	Créer un nouveau  lycée : <a href="" title="Créer un nouveau  lycée">Créer un nouveau  lycée</a>
+<br>
+2.	Modifier un lycée déjà crée : <a href="" title="Modifier un lycée déjà crée">Modifier un lycée déjà crée</a>
+<br>
+3.	Supprimer un lycée : <a href="" title="Supprimer un lycée">Supprimer un lycée</a>
+<br>
+4.	Configurer un lycée déjà crée : <a href="" title="Configurer un lycée déjà crée">Configurer un lycée déjà crée</a>
+</pre>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <span class="profile-ava">
+                                <img style="height:40px; width:40px" alt="icone" src="../public_files/img/iconSYGELYCO.png">
+                            </span>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Modal fin -->
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-green">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-tasks fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div style="font-family:'Times New Roman', Times, serif; font-size:22px" class="huge">Module 2</div>
+                                    <div>Année académique</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">Voir Détails</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-yellow">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-shopping-cart fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div style="font-family:'Times New Roman', Times, serif; font-size:22px" class="huge">Module 3</div>
+                                    <div>Personnels</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">Voir Détails</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-red">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-support fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div style="font-family:'Times New Roman', Times, serif; font-size:22px" class="huge">Module 4</div>
+                                    <div>Etudiants</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">Voir Détails</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
             </div>
 			
-				 <div class="col-md-6 portlets">
-              <div class="panel panel-default">
-                <div class="panel-heading">
-                  <div class="pull-left"><?php echo actualites; ?></div>
-                  <div class="widget-icons pull-right">
-                    <a href="#" class="wminimize"><i class="fa fa-chevron-up"></i></a> 
-                    <a href="#" class="wclose"><i class="fa fa-times"></i></a>
-                  </div>  
-                  <div class="clearfix"></div>
+			<!-- ligne 2 -->
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                  <div class="panel panel-nuit">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-tasks fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div style="font-family:'Times New Roman', Times, serif; font-size:22px" class="huge">Module 5</div>
+                                    <div>Bulletins</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">Voir Détails</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div> 
                 </div>
-                <div class="panel-body">
-                  <div class="padd">
-                  
-
-                  </div>
-                  <div class="widget-foot">
-                    <!-- Footer goes here -->
-                  </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-maron">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-tasks fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div style="font-family:'Times New Roman', Times, serif; font-size:22px" class="huge">Module 6</div>
+                                    <div>SMS</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">Voir Détails</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
                 </div>
-              </div>
-              
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-bleu2">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-shopping-cart fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div style="font-family:'Times New Roman', Times, serif; font-size:22px" class="huge">Module 7</div>
+                                    <div>Parametres</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">Voir Détails</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-gris">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-support fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div style="font-family:'Times New Roman', Times, serif; font-size:22px" class="huge">Module 4</div>
+                                    <div>Actualitées</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">
+                            <div class="panel-footer">
+                                <span class="pull-left">Voir Détails</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
             </div>
-                        
-          </div> 
               <!-- project team & activity end -->
 
           </section>
